6a50ac3b1330aa9cca463469ae7637745edc90cb
Anonymouss <mauludindayat@gmail.com>
2023-04-27 22:56:16 +0700

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href={"/images/myLogo.ico"} />
+        <link rel="icon" href={"/images/myLogo.ico?v=2"} />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
