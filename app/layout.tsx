8c782c647cc473dd25b775418bad7440dfc8260d
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2023-12-19 13:51:15 +0700

    fix logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,6 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="My Portfolio" />
         <link
-          rel="shortcut icon"
-          href="./favicon.ico"
-          type="image/x-icon"
-          sizes="any"
-        />
-        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
